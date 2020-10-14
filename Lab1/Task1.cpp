27f15ce98993a1ab0174275f9a956a8641efcd70
nextprof <kubabednarowicz8@gmail.com>
2020-10-14 14:31:57 +0200

    Final version of Task1

--- Lab1/Task1.cpp
+++ Lab1/Task1.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
-int main()
+using namespace std;
+
+void bubble_sort(char** tab, int n)
 {
-    std::cout <<"hello world WOHOOOOO";
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = 1; j < n-1; j++)
+		{
+			if (atoi(tab[j]) > atoi(tab[j + 1]))
+			{
+                swap(tab[j],tab[j+1]);
+			}
+		}
+	}
+    cout << "out = ";
+    for (int i = 1; i < n; i++)
+    {
+        cout << tab[i] << " ";
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    bubble_sort(argv,argc);
+
+    return 0;
 }
