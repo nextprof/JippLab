6770c66b8741223c675798772136c1b9a74e5dfa
nextprof <kubabednarowicz8@gmail.com>
2020-10-20 13:56:45 +0200

    Task 1 - swap

---
+++ Lab2/Task1-swap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void My_swap(int *a, int *b)
+{
+    if (*b < *a)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
+
+int main()
+{
+    int a, b;
+    cout << "Podaj kolejno 2 liczby: " << endl;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+
+    My_swap(&a, &b);
+
+    cout << "a: " << a << "  b: " << b;
+    return 0;
+}
