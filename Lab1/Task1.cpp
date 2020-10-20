3addefebe4f6b1ba23bab41fa00b135eded553e5
nextprof <kubabednarowicz8@gmail.com>
2020-10-20 13:30:04 +0200

    Added if argc==1 - Sortowanie ----- Code clearer

--- Lab1/Task1.cpp
+++ Lab1/Task1.cpp
@@ -2,29 +2,33 @@
 
 using namespace std;
 
-void bubble_sort(char** tab, int n)
+void bubble_sort(char **tab, int n)
 {
-	for (int i = 0; i < n-1; i++)
+	for (int i = 0; i < n - 1; i++)
 	{
-		for (int j = 1; j < n-1; j++)
+		for (int j = 1; j < n - 1; j++)
 		{
 			if (atoi(tab[j]) > atoi(tab[j + 1]))
 			{
-                swap(tab[j],tab[j+1]);
+				swap(tab[j], tab[j + 1]);
 			}
 		}
 	}
-    cout << "out = ";
-    for (int i = 1; i < n; i++)
-    {
-        cout << tab[i] << " ";
-    }
+	cout << "out = ";
+	for (int i = 1; i < n; i++)
+	{
+		cout << tab[i] << " ";
+	}
 }
 
 int main(int argc, char *argv[])
 {
+	if (argc == 1)
+	{
+		cout << "Empty input";
+		return 0;
+	}
+	bubble_sort(argv, argc);
 
-    bubble_sort(argv,argc);
-
-    return 0;
+	return 0;
 }
--- Lab1/Task3.cpp
+++ Lab1/Task3.cpp
@@ -7,19 +7,28 @@ void BMI_Check()
 {
     double wzrost, waga, BMI;
     cout << "---- Obliczanie BMI ----" << endl;
-    cout << "Podaj wzrost [m] :"; cin >> wzrost;
-    cout << "Podaj mase ciala [kg] :"; cin >> waga;
-    BMI = (waga)/(wzrost * wzrost);
-    if(BMI<16) cout << "Twoje BMI to - wyglodzenie";
-    else if((BMI>=16)&&(BMI<17)) cout << "Twoje BMI to - wychudzenie";
-    else if((BMI>=17)&&(BMI<18.5)) cout << "Twoje BMI to - niedowaga";
-    else if((BMI>=18.5)&&(BMI<25)) cout << "Twoje BMI to - wartosc prawidlowa";
-    else if((BMI>=25)&&(BMI<30)) cout << "Twoje BMI to - nadwaga";
-    else if((BMI>=30)&&(BMI<35)) cout << "Twoje BMI to - I stopien otylosci";
-    else if((BMI>=35)&&(BMI<40)) cout << "Twoje BMI to - II stopien otylosci";
-    else cout << "Twoje BMI to - otylosc skrajna";
+    cout << "Podaj wzrost [m] :";
+    cin >> wzrost;
+    cout << "Podaj mase ciala [kg] :";
+    cin >> waga;
+    BMI = (waga) / (wzrost * wzrost);
+    if (BMI < 16)
+        cout << "Twoje BMI to - wyglodzenie";
+    else if ((BMI >= 16) && (BMI < 17))
+        cout << "Twoje BMI to - wychudzenie";
+    else if ((BMI >= 17) && (BMI < 18.5))
+        cout << "Twoje BMI to - niedowaga";
+    else if ((BMI >= 18.5) && (BMI < 25))
+        cout << "Twoje BMI to - wartosc prawidlowa";
+    else if ((BMI >= 25) && (BMI < 30))
+        cout << "Twoje BMI to - nadwaga";
+    else if ((BMI >= 30) && (BMI < 35))
+        cout << "Twoje BMI to - I stopien otylosci";
+    else if ((BMI >= 35) && (BMI < 40))
+        cout << "Twoje BMI to - II stopien otylosci";
+    else
+        cout << "Twoje BMI to - otylosc skrajna";
 }
-
 
 int main(int argc, char *argv[])
 {
